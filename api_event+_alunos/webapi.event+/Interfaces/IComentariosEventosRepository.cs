a48bd1d9fd08a2b8a8020867e7ddc0d46954b350
Carlos Roque <carlos.roque@sp.senai.br>
2025-03-25 11:55:03 -0300

    feat: criado projeto, domains, contexto, interfaces e repositórios, falta controllers, criptografia e IA

---
+++ api_event+_alunos/webapi.event+/Interfaces/IComentariosEventosRepository.cs
@@ -0,0 +1,12 @@
+﻿using webapi.event_.Domains;
+
+namespace webapi.event_.Interfaces
+{
+    public interface IComentariosEventosRepository
+    {
+        void Cadastrar(ComentariosEventos comentarioEvento);
+        void Deletar(Guid id);
+        List<ComentariosEventos> Listar(Guid id);
+        ComentariosEventos BuscarPorIdUsuario(Guid idUsuario, Guid idEvento);
+    }
+}
---
+++ api_event+_alunos/webapi.event+/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using webapi.event_.Domains;
+
+namespace webapi.event_.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        void Cadastrar(Usuarios usuario);
+
+        Usuarios BuscarPorId(Guid id);
+
+        Usuarios BuscarPorEmailESenha(string email, string senha);
+    }
+}
